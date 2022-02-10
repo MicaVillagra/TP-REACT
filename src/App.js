c51c17d2c84f7eff11f1d3958fca679de8202590
Micaela Villagra <micavillagrag@hotmail.com>
2022-02-10 18:58:25 -0300

    primertp

--- src/App.js
+++ src/App.js
@@ -4,20 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hola Mundo de React!</h1>
+      <h2>Jugando con React y Css</h2>
     </div>
   );
 }
